7f7267171699120c6c1ff7b11abba4adc0e16e1f
Ainur <ismy1286@gmail.com>
2021-12-23 14:56:41 +0600

    first commit

--- FirstTask/ViewController.swift
+++ FirstTask/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+    private var textField = UITextField()
+    private var button = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.placeholder = "Введите текст..."
+        
+        view.addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-25)
+            make.width.equalToSuperview().offset(-100)
+        }
+        
+        button.backgroundColor = .yellow
+        button.setTitle("Print", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(nil, action: #selector(clickButton(view:)), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(25)
+            make.width.equalTo(100)
+        }
     }
-
-
+    @objc func clickButton(view: UIButton) {
+        let secondText = SecondScreen()
+        secondText.s = textField.text ?? ""
+        
+        navigationController?.pushViewController(secondText, animated: true)
+    }
 }
 
