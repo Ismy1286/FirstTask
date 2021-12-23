41d575424459017ee364fce6efc42f74b3dfbfec
Ainur <ismy1286@gmail.com>
2021-12-23 14:57:40 +0600

    commit

--- FirstTask/SecondScreen.swift
+++ FirstTask/SecondScreen.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+
+class SecondScreen: UIViewController {
+    
+    var s: String = ""
+    var textLabel = UILabel()
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .yellow
+        
+        textLabel.text = s
+        textLabel.numberOfLines = 0
+        textLabel.lineBreakMode = .byWordWrapping
+    
+        view.addSubview(textLabel)
+        textLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview().offset(-50)
+        }
+    }
+}
